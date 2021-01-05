ac930c32bcee87c7e932999cf811cdba92d9a969
Ana Luisa Dias <analudias@live.com>
2021-01-04 23:06:29 -0300

    Importing data to table

---
+++ src/main/java/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_products (date, description, name, price) VALUES (TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.', 'The Lord of the Rings', 90.5);
+
+INSERT INTO tb_products (date, description, name, price) VALUES (TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.', 'As Cronicas de Narnia', 15);
+
+INSERT INTO tb_products (date, description, name, price) VALUES (TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.', 'A Cabana', 25);
+
+INSERT INTO tb_products (date, description, name, price) VALUES (TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.', 'A Marca de uma Lagrima', 37.5);
+
+INSERT INTO tb_products (date, description, name, price) VALUES (TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.', 'A Droga da Obediencia', 22);
+
+INSERT INTO tb_products (date, description, name, price) VALUES (TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum.', 'Agora Estou Sozinha', 31);
